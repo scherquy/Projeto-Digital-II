5188568a036df93651cf2d80151746473946f7f1
scherquy <jschervensquy@gmail.com>
2025-11-20 13:51:31 -0300

    Update monociclo.vhd

--- Monociclo/monociclo.vhd
+++ Monociclo/monociclo.vhd
@@ -10,6 +10,7 @@ port(
 end entity;
 
 architecture behavior of monociclo is
+	--PC e MUXES para o PC
 	signal PC : std_logic_vector (7 downto 0); --sinal para o PC
 	signal mux_branch, mux_jump : std_logic_vector (7 downto 0); --sinais para os muxes de saltos para o PC
 
@@ -23,15 +24,35 @@ architecture behavior of monociclo is
 
 	signal memoria_dados : memoria; --memoria de dados
 	signal memoria_dados_out : std_logic_vector (19 downto 0); --saida da memoria de dados
-
-  --operacoes e saida da ULA
+	
+	--operacoes e saida da ULA
 	signal saida_ula : std_logic_vector(3 downto 0);
 	signal soma, sub : std_logic_vector (3 downto 0);
 	signal mult : std_logic_vector (7 downto 0);
 	
 begin
+
+memoria_instrucoes_out <= memoria_instrucoes(conv_integer(PC));
+opcode <= memoria_instrucoes_out(19 downto 16);
+
+--tipo R
+reg_1 <= memoria_instrucoes_out(15 downto 12) when opcode = "0001" or opcode = "0010" or opcode = "0011" else
+	(others => '0');
+reg_2 <= memoria_instrucoes_out(11 downto 8) when opcode = "0001" or opcode = "0010" or opcode = "0011" else
+	(others => '0');
+reg_destino <= memoria_instrucoes_out(7 downto 4) when opcode = "0001" or opcode = "0010" or opcode = "0011" else
+	(others => '0');
+	
+		
+
+
 	process(reset, clock)
 	begin
+		if(reset = '0') then
+			PC <= (others => '0');
+		elsif(clock = '1' and clock'event) then
+			PC <= PC + 1;
+		end if;
 	end process;
 
 end architecture;
