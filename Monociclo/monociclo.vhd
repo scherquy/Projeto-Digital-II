7a53dc352fe5b89f4b3206a61ab10f78b5cc6e0e
Rafael <rafaeloliva2002@outlook.com>
2025-11-26 15:40:45 -0300

    primeiro versão do monociclo compilando.

--- Monociclo/monociclo.vhd
+++ Monociclo/monociclo.vhd
@@ -30,10 +30,10 @@ architecture behavior of monociclo is
     type memoria_dados_t is array (integer range 0 to 255) of std_logic_vector (15 downto 0);
     signal memoria_dados       : memoria_dados_t;
     signal memoria_dados_out   : std_logic_vector (15 downto 0);
-    signal endereco_mem        : std_logic_vector (8 downto 0);
+    signal endereco_mem        : std_logic_vector (15 downto 0);
 
     -- BANCO DE REGISTRADORES DO MIPS
-    type registradores is array (integer range 0 to 15) of std_logic_vector(15 downto 0);
+    type registradores is array (integer range 0 to 15) of std_logic_vector(15 downto 0); -- indices sendo representador por inteiros por isso a conversao
     signal banco_reg : registradores;
 
     -- SINAIS PARA ULA
@@ -41,6 +41,7 @@ architecture behavior of monociclo is
     signal soma      : std_logic_vector (15 downto 0);
     signal sub       : std_logic_vector (15 downto 0);
     signal mult      : std_logic_vector (31 downto 0);
+    signal muli	     : std_logic_vector (31 downto 0);
     signal equal     : std_logic;
 
     -- VALOR CARREGADO DOS REGISTRADORES
@@ -54,6 +55,9 @@ begin
 
     -- buscar instrução
     memoria_instrucoes_out <= memoria_instrucoes(conv_integer(PC));
+
+    -- buscar dados da memoria
+    memoria_dados_out <= offset_ext + imediato;
 
     -- decodifica campos
     opcode  <= memoria_instrucoes_out(19 downto 16);
@@ -73,11 +77,13 @@ begin
     soma <= valor_rs + valor_rt;
     sub  <= valor_rs - valor_rt;
     mult <= valor_rs * valor_rt;
+    muli <= valor_rs * offset_ext;
     equal<= '1' when valor_rs = valor_rt else '0';
 
     saida_ula <= soma when opcode = "0001" else
                  sub  when opcode = "0010" else
                  mult(15 downto 0) when opcode = "0011" else
+		 muli(15 downto 0) when opcode = "0111" else
                  (others => '0');
 
     -- calcular endereco_mem 
@@ -115,12 +121,12 @@ begin
                     end if;
 
                 ---------- TIPO I -------------
-                when "0100" => -- LDI rt <- imediato (zero-extend)
+                when "0100" => -- LDI rt <- imediato 
                     if reg_rt /= "0000" then
                         banco_reg(conv_integer(reg_rt)) <= offset_ext;
                     end if;
 
-                when "0101" => -- ADDI rt <- rs + imd (zero-extend)
+                when "0101" => -- ADDI rt <- rs + imd 
                     if reg_rt /= "0000" then
                         banco_reg(conv_integer(reg_rt)) <= valor_rs + offset_ext;
                     end if;
@@ -132,34 +138,38 @@ begin
 
                 when "0111" => -- MULI rt <- rs * imd
                     if reg_rt /= "0000" then
-                        banco_reg(conv_integer(reg_rt)) <= valor_rs * offset_ext;
+                        banco_reg(conv_integer(reg_rt)) <= saida_ula;
                     end if;
 
                 when "1000" => -- LW rt <- Mem[rs + imd]
                     if reg_rt /= "0000" then
-                        banco_reg(conv_integer(reg_rt)) <= memoria_dados(conv_integer(endereco_mem));
+  		
+			banco_reg(conv_integer(reg_rt)) <= memoria_dados_out;                  
+		    
                     end if;
 
                 when "1001" => -- SW Mem[rs + imd] <- rt
-                    memoria_dados(conv_integer(endereco_mem)) <= valor_rt;
+		
+		memoria_dados(conv_integer(endereco_mem)) <= valor_rt;
+                    
 
                 ---------- TIPO J -------------
                 when "1010" => -- JMP PC <- IMD
                     PC <= imediato; -- imediato tem 8 bits; PC tem 8 bits
 
-                when "1011" => -- BEQ (PC <- PC + offset se igual)
+                when "1011" => -- BEQ (PC <- PC + imd)
                     if equal = '1' then
-                        PC <= PC + offset_ext;
+                        PC <= PC + imediato;
                     end if;
 
                 when "1100" => -- BNE
                     if equal = '0' then
-                        PC <= PC + offset_ext;
+                        PC <= PC + imediato;
 		     end if;
 
                 when others =>
                     null;
-            end case; -- case opcode
+            end case; 
         end if;
     end process;
 
